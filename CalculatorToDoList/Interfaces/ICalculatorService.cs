b22833b5a8a8e8e5387b7939d6693b4bd77722fd
Ankita <A00305054@mycambrian.ca>
2024-10-03 21:10:15 -0400

    created interface calculator servive

--- CalculatorToDoList/Interfaces/ICalculatorService.cs
+++ CalculatorToDoList/Interfaces/ICalculatorService.cs
@@ -1,10 +1,1 @@
-﻿namespace CalculatorWebAPI.Interfaces
-{
-    public interface ICalculatorService<T>
-    {
-        T Add(T num1, T num2);
-        T Subtract(T num1, T num2);
-        T Multiply(T num1, T num2);
-        T Divide(T num1, T num2);
-    }
-}
+﻿
